ea7f06bf8c86000fe3286ba523f0c71bdc5dc277
Akos Eros <aeros@redhat.com>
2025-05-09 21:50:09 +0200

    WIP(csi-driver): Implement directory based volume provison
    Change dockerfile to ubi9 from distroless to have some utility
    available. (mountpoint binary)
    Add awsomefs host mount as bidirectional to all csi-driver containers
    (added privilege for this)

--- cmd/csi-awsomefs/driver/controller.go
+++ cmd/csi-awsomefs/driver/controller.go
@@ -2,30 +2,85 @@ package driver
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (d *Driver) CreateVolume(ctx context.Context, req *csi.CreateVolumeRequest) (*csi.CreateVolumeResponse, error) {
+	// --- Check if basePath is a mountpoint ---
+	if err := checkMountpoint(baseMountPath); err != nil {
+		return nil, fmt.Errorf("base path %s is not mounted: %v", baseMountPath, err)
+	}
+
 	volID := req.GetName()
-	log.Printf("CreateVolume called for: %s\n", volID)
+	if volID == "" {
+		return nil, status.Error(codes.InvalidArgument, "volume name is required")
+	}
+
+	capacity := req.GetCapacityRange().GetRequiredBytes()
+	if capacity == 0 {
+		capacity = int64(1 << 30) // Default to 1 GiB
+	}
+	volumePath := filepath.Join(baseMountPath, "volumes", volID)
+
+	log.Printf("CreateVolume called for: %s (capacity: %d bytes)", volID, capacity)
+
+	// Ensure volume directory exists
+	if _, err := os.Stat(volumePath); err == nil {
+		log.Printf("Volume %s already exists at %s", volID, volumePath)
+	} else if os.IsNotExist(err) {
+		if err := os.MkdirAll(volumePath, 0755); err != nil {
+			return nil, status.Errorf(codes.Internal, "failed to create volume directory: %v", err)
+		}
+		log.Printf("Created volume directory: %s", volumePath)
+	} else {
+		return nil, status.Errorf(codes.Internal, "failed to stat volume path: %v", err)
+	}
 
 	// TODO: Actually provision the volume somewhere (e.g., mkdir, thin file, etc.)
 	volume := &csi.Volume{
 		VolumeId:      volID,
-		CapacityBytes: req.GetCapacityRange().GetRequiredBytes(), // stubbed
-		VolumeContext: req.GetParameters(),
+		CapacityBytes: capacity, // stubbed
+		VolumeContext: map[string]string{
+			"mountPath": volumePath,
+		},
 	}
 
 	return &csi.CreateVolumeResponse{Volume: volume}, nil
 }
 
 func (d *Driver) DeleteVolume(ctx context.Context, req *csi.DeleteVolumeRequest) (*csi.DeleteVolumeResponse, error) {
-	volID := req.GetVolumeId()
-	log.Printf("DeleteVolume called for: %s\n", volID)
 
-	// TODO: Actually delete the volume from backend
+	// --- Sanity check: is baseMountPath mounted? ---
+	if err := checkMountpoint(baseMountPath); err != nil {
+		return nil, fmt.Errorf("base mount path %s is not mounted: %v", baseMountPath, err)
+	}
+
+	volID := req.GetVolumeId()
+	if volID == "" {
+		return nil, fmt.Errorf("volume ID is required")
+	}
+
+	volumePath := filepath.Join(baseMountPath, "volumes", volID)
+
+	if _, err := os.Stat(volumePath); os.IsNotExist(err) {
+		log.Printf("DeleteVolume: volume path %s does not exist, treating as deleted", volumePath)
+		// Volume already deleted, return success
+		return &csi.DeleteVolumeResponse{}, nil
+	}
+
+	if err := os.RemoveAll(volumePath); err != nil {
+		return nil, fmt.Errorf("failed to delete volume directory %s: %v", volumePath, err)
+	}
+
+	log.Printf("DeleteVolume: successfully deleted %s", volumePath)
 	return &csi.DeleteVolumeResponse{}, nil
 }
 
@@ -55,4 +110,12 @@ func (d *Driver) ControllerUnpublishVolume(
 	req *csi.ControllerUnpublishVolumeRequest,
 ) (*csi.ControllerUnpublishVolumeResponse, error) {
 	return &csi.ControllerUnpublishVolumeResponse{}, nil
+}
+
+func checkMountpoint(path string) error {
+	cmd := exec.Command("mountpoint", "-q", path)
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("not a mountpoint or inaccessible: %v", err)
+	}
+	return nil
 }
--- cmd/csi-awsomefs/driver/node.go
+++ cmd/csi-awsomefs/driver/node.go
@@ -3,9 +3,10 @@ package driver
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
-	"strings"
+	"path/filepath"
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
 )
@@ -17,10 +18,22 @@ func (d *Driver) NodePublishVolume(
 	req *csi.NodePublishVolumeRequest,
 ) (*csi.NodePublishVolumeResponse, error) {
 
-	volID := req.GetVolumeId()
-	targetPath := req.GetTargetPath()
+	// --- Sanity check: is baseMountPath mounted? ---
+	if err := checkMountpoint(baseMountPath); err != nil {
+		return nil, fmt.Errorf("base mount path %s is not mounted: %v", baseMountPath, err)
+	}
 
-	sourcePath := fmt.Sprintf("%s/%s", baseMountPath, volID)
+	volID := req.GetVolumeId()
+	if volID == "" {
+		return nil, fmt.Errorf("volume ID is required")
+	}
+
+	targetPath := req.GetTargetPath()
+	if targetPath == "" {
+		return nil, fmt.Errorf("target path is required")
+	}
+
+	sourcePath := filepath.Join(baseMountPath, "volumes", volID)
 
 	// Check that source path exists
 	if _, err := os.Stat(sourcePath); os.IsNotExist(err) {
@@ -33,17 +46,21 @@ func (d *Driver) NodePublishVolume(
 	}
 
 	// Check if already mounted
-	cmd := exec.Command("mount", "--bind", sourcePath, targetPath)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		// If the error is because it's already mounted, it's fine
-		if strings.Contains(string(output), "already mounted") {
-			fmt.Println("Volume already mounted.")
-		} else {
-			return nil, fmt.Errorf("failed to bind mount: %v, output: %s", err, string(output))
-		}
+	cmd := exec.Command("mountpoint", "-q", targetPath)
+	if err := cmd.Run(); err == nil {
+		// Already mounted
+		log.Printf("Volume %s already mounted at %s", volID, targetPath)
+		return &csi.NodePublishVolumeResponse{}, nil
 	}
 
+	// Perform the bind mount
+	cmd = exec.Command("mount", "--bind", sourcePath, targetPath)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return nil, fmt.Errorf("failed to bind mount: %v, output: %s", err, string(output))
+	}
+
+	log.Printf("Successfully bind-mounted %s to %s", sourcePath, targetPath)
 	return &csi.NodePublishVolumeResponse{}, nil
 }
 
@@ -52,18 +69,32 @@ func (d *Driver) NodeUnpublishVolume(
 	req *csi.NodeUnpublishVolumeRequest,
 ) (*csi.NodeUnpublishVolumeResponse, error) {
 
-	targetPath := req.GetTargetPath()
+	// --- Sanity check: is baseMountPath mounted? ---
+	if err := checkMountpoint(baseMountPath); err != nil {
+		return nil, fmt.Errorf("base mount path %s is not mounted: %v", baseMountPath, err)
+	}
 
-	// Unmount volume
-	cmd := exec.Command("umount", targetPath)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		// Check if it's already unmounted
-		if strings.Contains(string(output), "not mounted") {
-			fmt.Println("Volume is already unmounted.")
-		} else {
-			return nil, fmt.Errorf("failed to unmount: %v, output: %s", err, string(output))
+	targetPath := req.GetTargetPath()
+	if targetPath == "" {
+		return nil, fmt.Errorf("target path is required")
+	}
+
+	// Check if target path is a mountpoint
+	cmd := exec.Command("mountpoint", "-q", targetPath)
+	if err := cmd.Run(); err != nil {
+		// Not a mountpoint, assume already unmounted
+		log.Printf("Target path %s is not a mountpoint or already unmounted", targetPath)
+	} else {
+		// Unmount it
+		if err := exec.Command("umount", targetPath).Run(); err != nil {
+			return nil, fmt.Errorf("failed to unmount %s: %v", targetPath, err)
 		}
+		log.Printf("Successfully unmounted %s", targetPath)
+	}
+
+	// Optionally remove the target directory
+	if err := os.RemoveAll(targetPath); err != nil {
+		return nil, fmt.Errorf("failed to remove target path %s: %v", targetPath, err)
 	}
 
 	return &csi.NodeUnpublishVolumeResponse{}, nil
